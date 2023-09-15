62b43de12b6224eab8a83f9aea1b467fe32cf5c5
VedC <vedvarunc@gmail.com>
2023-09-15 09:00:29 -0400

    TablesUpTo65536

---
+++ Exercises/ch06/Tables.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main(){
+
+    double x = 1.0;
+    while (x < 66536.0) {
+        cout << x << "\t" << log(x) / log(2.0) << endl;
+        x = x * 2.0;
+    }
+}
