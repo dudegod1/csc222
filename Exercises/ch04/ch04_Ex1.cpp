7936ae0a46c9d01704fca5d5278151a0bbf4a40c
VedC <vedvarunc@gmail.com>
2023-09-15 12:37:34 -0400

    Goes_to_-261501

---
+++ Exercises/ch04/ch04_Ex1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+void countdown(int n) {
+    if (n == 0) {
+        cout << "Blastoff!" << endl;
+    } else {
+        cout << n << endl;
+        countdown(n-1);
+    }
+}
+
+int main(){
+    countdown(1);
+}
