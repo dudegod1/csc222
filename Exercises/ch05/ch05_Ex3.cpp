6cbf6b20a5a978d09dec5faae0c3701fbaf79718
VedC <vedvarunc@gmail.com>
2023-09-15 12:24:40 -0400

    ch05_Ex3_5functions

--- Exercises/ch05/ch05_Ex3.cpp
+++ Exercises/ch05/ch05_Ex3.cpp
@@ -6,6 +6,22 @@ void overloaded_function(int a){
     cout << a+1 << endl;
 }
 
+void overloaded_function(int a,int b){
+    cout << a+2 << endl;
+}
+void overloaded_function(int a,int b,int c){
+    cout << a+3 << endl;
+}
+void overloaded_function(int a,int b,int c,int d){
+    cout << a+4 << endl;
+}
+void overloaded_function(int a,int b,int c,int d,int e){
+    cout << a+5 << endl;
+}
 int main(){
     overloaded_function(1);
+    overloaded_function(1,2);
+    overloaded_function(1,2,3);
+    overloaded_function(1,2,3,4);
+    overloaded_function(1,2,3,4,5);
 }
