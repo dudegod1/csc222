a61ba4b71f1995c3c8d9f0e81cfea008da288ccd
VedC <vedvarunc@gmail.com>
2023-12-07 00:03:32 -0500

    point_prob

---
+++ Exercises/ch15/test_point.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+#include "Point.h"
+
+using namespace std;
+
+int main(){
+    Point p(2,3);
+    cout << p.to_string << endl;    
+    
+    return 0;
+}
