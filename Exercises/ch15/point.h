f6f07d7e9b606fd8be35b076e77abf417cebc0fe
VedC <vedvarunc@gmail.com>
2023-12-09 23:12:58 -0500

    point_h

--- Exercises/ch15/point.h
+++ Exercises/ch15/point.h
@@ -5,11 +5,15 @@
 using namespace std;
 
 struct Point {
-    double x,y;
+    int x,y;
     
     Point();
-    Point(c1, c2);
-    string to_string();
+    Point(int c1, int c2);
+    string to_string() const;
     Point  operator + (const Point& c);
     Point  operator - (const Point& c);
+    Point operator * (int val);
+    double distance(Point& p2);
 };
+
+double distance(Point& p1, Point& p2);
