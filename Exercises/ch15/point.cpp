de2d69f5bafecf792229e1851ca4939d8259269b
VedC <vedvarunc@gmail.com>
2023-12-09 23:15:39 -0500

    pointcpp

--- Exercises/ch15/point.cpp
+++ Exercises/ch15/point.cpp
@@ -9,11 +9,34 @@ Point:: Point(){
     this->y = 0;
 }
 
-Point::Point(double c1, double c2){
+Point::Point(int c1,int c2){
     this->x = c1;
     this->y = c2;
 }
 
-string Point :: to_string(){
-    return x.to_string() + ", " + y.to_string();
+string Point::to_string() const{
+   return "("+ std::to_string(x) + ", " + std::to_string(y) + ")";
+}
+
+Point Point::operator + (const Point& p){
+    Point p3(this->x + p.x, this->y + p.y);
+    return p3;
+}
+Point Point::operator - (const Point& p){
+    Point p3(this->x -  p.x, this->y -  p.y);
+    return p3;
+}
+
+Point Point::operator * (int val){
+    Point p3(this->x * val, this-> y * val);
+    return p3;
+}
+
+double Point::distance(Point& p2){
+    return double(sqrt(pow(this->x + p2.x, 2) + pow(this->y + p2.y, 2)));
+}
+
+    
+double distance(Point& p1, Point& p2){
+    return double(sqrt(pow(p1.x + p2.x, 2) + pow(p1.y + p2.y, 2))); 
 }
